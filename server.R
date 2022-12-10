0a473b21e2f34a4df9210f28c37074a08536277b
Brant <brant.j.armstrong@gmail.com>
2022-12-10 17:28:54 -0500

    same

--- server.R
+++ server.R
@@ -7,11 +7,306 @@
 #    http://shiny.rstudio.com/
 #
 
+library(tidyverse)
+library(lubridate)
+library(DT)
+library(psych)
 library(shiny)
+library(shinydashboard)
+library(DescTools)
+library(summarytools)
 
-# Define server logic required to draw a histogram
+
+coffee_ratings <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv')
+
+vec_remove_variables = c("owner", "farm_name", "lot_number","mill", "ico_number",
+                         "company", "altitude", "region", "producer", "number_of_bags", "bag_weight", 
+                         "in_country_partner", "harvest_year", "owner_1","certification_body", "certification_address",
+                         "certification_contact", "unit_of_measurement", "altitude_low_meters", "altitude_high_meters")
+vec_factors = c("species", "country_of_origin","variety", "processing_method", "color", "day")
+
+df_coffee <- coffee_ratings %>% 
+  #Removing non-predictive variables
+  select(-vec_remove_variables) %>% 
+  #Creating a day variable for when coffee graded
+  mutate(day = wday(mdy(grading_date), label = TRUE, abbr = FALSE)) %>% 
+  mutate(year = year(mdy(grading_date))) %>%
+  #Removing unneeded variables
+  select(-c("grading_date","expiration")) %>%
+  #Changing categorical variables to factors
+  mutate(across(vec_factors, factor)) %>%
+  #Removing incomplete observations
+  drop_na() %>%
+  #Removing impossible altitude values
+  filter(altitude_mean_meters < 4200) %>%
+  filter(species == "Arabica") %>%
+  select(-species)
+
+
+
 shinyServer(function(input, output) {
-
-
-
+  
+  output$scorePlot <- renderPlot({
+    g_score <- ggplot(df_coffee, aes(x = total_cup_points)) +
+      geom_histogram()
+    g_score
+  })
+  output$scoreTable <- renderTable({
+    tab_score <- describe(df_coffee$total_cup_points, fast = TRUE) %>%
+                select(-vars)
+    
+  })
+  
+  output$varietyPlot <- renderPlot({
+    g_variety <- ggplot(df_coffee, aes(x = variety)) +
+      geom_bar() +
+      theme(axis.text.x = element_text(angle = 45))
+    g_variety
+  })
+  output$varietyTable <- renderTable({
+    tab_variety <- table(df_coffee$variety)
+    tab_variety
+  })
+  
+  output$procPlot <- renderPlot({
+    g_proc <- ggplot(df_coffee, aes(x = processing_method)) +
+      geom_bar() +
+      theme(axis.text.x = element_text(angle = 45))
+    g_proc
+  })
+  
+  output$procTable <- renderTable({
+    tab_proc <- table(df_coffee$processing_method)
+    tab_proc
+  })
+  
+  output$aromaPlot <- renderPlot({
+    g_aroma <- ggplot(df_coffee, aes(x = aroma)) +
+      geom_histogram() 
+    g_aroma
+  })
+  
+  output$aromaTable <- renderTable({
+    tab_aroma <- describe(df_coffee$aroma, fast = TRUE) %>%
+      select(-vars)
+    tab_aroma
+  })
+  
+  output$flavorPlot <- renderPlot({
+    g_flavor <- ggplot(df_coffee, aes(x = flavor)) +
+      geom_histogram() 
+    g_flavor
+  })
+  
+  output$flavorTable <- renderTable({
+    tab_flavor <- describe(df_coffee$flavor, fast = TRUE) %>%
+      select(-vars)
+    tab_flavor
+  })
+  
+  output$tastePlot <- renderPlot({
+    g_taste <- ggplot(df_coffee, aes(x = aftertaste)) +
+      geom_histogram() 
+    g_taste
+  })
+  
+  output$tasteTable <- renderTable({
+    tab_taste <- describe(df_coffee$aftertaste, fast = TRUE) %>%
+      select(-vars)
+    tab_taste
+  })
+  
+  output$acidPlot <- renderPlot({
+    g_acid <- ggplot(df_coffee, aes(x = acidity)) +
+      geom_histogram() 
+    g_acid
+  })
+  
+  output$acidTable <- renderTable({
+    tab_acid <- describe(df_coffee$acidity, fast = TRUE) %>%
+      select(-vars)
+    tab_acid
+  })
+  
+  output$bodyPlot <- renderPlot({
+    g_body <- ggplot(df_coffee, aes(x = body)) +
+      geom_histogram() 
+    g_body
+  })
+  
+  output$bodyTable <- renderTable({
+    tab_body <- describe(df_coffee$body, fast = TRUE) %>%
+      select(-vars)
+    tab_body
+  })
+  
+  output$balancePlot <- renderPlot({
+    g_balance <- ggplot(df_coffee, aes(x = balance)) +
+      geom_histogram() 
+    g_balance
+  })
+  
+  output$balanceTable <- renderTable({
+    tab_balance <- describe(df_coffee$balance, fast = TRUE) %>%
+      select(-vars)
+    tab_balance
+  })
+  
+  
+  output$uniformPlot <- renderPlot({
+    g_uniform <- ggplot(df_coffee, aes(x = uniformity)) +
+      geom_histogram(bins=30) 
+    g_uniform
+  })
+  
+  output$uniformTable <- renderTable({
+    tab_uniform <- describe(df_coffee$uniformity, fast = TRUE) %>%
+      select(-vars)
+    tab_uniform
+  })
+  
+  output$sweetPlot <- renderPlot({
+    g_sweet <- ggplot(df_coffee, aes(x = sweetness)) +
+      geom_histogram() 
+    g_sweet
+  })
+  
+  output$sweetTable <- renderTable({
+    tab_sweet <- describe(df_coffee$sweetness, fast = TRUE) %>%
+      select(-vars)
+    tab_sweet
+  })
+  
+  output$cleanPlot <- renderPlot({
+    g_clean <- ggplot(df_coffee, aes(x = clean_cup)) +
+      geom_histogram() 
+    g_clean
+  })
+  
+  output$cleanTable <- renderTable({
+    tab_clean <- describe(df_coffee$clean_cup, fast = TRUE) %>%
+      select(-vars)
+    tab_clean
+  })
+  
+  output$cleanPlot <- renderPlot({
+    g_clean <- ggplot(df_coffee, aes(x = clean_cup)) +
+      geom_histogram() 
+    g_clean
+  })
+  
+  output$cleanTable <- renderTable({
+    tab_clean <- describe(df_coffee$clean_cup, fast = TRUE) %>%
+      select(-vars)
+    tab_clean
+  })
+  
+  output$pointsPlot <- renderPlot({
+    g_points <- ggplot(df_coffee, aes(x = cupper_points)) +
+      geom_histogram() 
+    g_points
+  })
+  
+  output$pointsTable <- renderTable({
+    tab_points <- describe(df_coffee$cupper_points, fast = TRUE) %>%
+      select(-vars)
+    tab_points
+  })
+  
+  output$moistPlot <- renderPlot({
+    g_moist <- ggplot(df_coffee, aes(x = moisture)) +
+      geom_histogram() 
+    g_moist
+  })
+  
+  output$moistTable <- renderTable({
+    tab_moist <- describe(df_coffee$moisture, fast = TRUE) %>%
+      select(-vars)
+    tab_moist
+  })
+  
+  output$cat1Plot <- renderPlot({
+    g_cat1 <- ggplot(df_coffee, aes(x = category_one_defects)) +
+      geom_histogram() 
+    g_cat1
+  })
+  
+  output$cat1Table <- renderTable({
+    tab_cat1 <- describe(df_coffee$category_one_defects, fast = TRUE) %>%
+      select(-vars)
+    tab_cat1
+  })
+  
+  
+  output$cat2Plot <- renderPlot({
+    g_cat2 <- ggplot(df_coffee, aes(x = category_two_defects)) +
+      geom_histogram() 
+    g_cat2
+  })
+  
+  output$cat2Table <- renderTable({
+    tab_cat2 <- describe(df_coffee$category_two_defects, fast = TRUE) %>%
+      select(-vars)
+    tab_cat2
+  })
+  
+  
+  output$quakersPlot <- renderPlot({
+    g_quakers <- ggplot(df_coffee, aes(x = quakers)) +
+      geom_histogram() 
+    g_quakers
+  })
+  
+  output$quakersTable <- renderTable({
+    tab_quakers <- describe(df_coffee$quakers, fast = TRUE) %>%
+      select(-vars)
+    tab_quakers
+  })
+  
+  
+  output$colorPlot <- renderPlot({
+    g_color <- ggplot(df_coffee, aes(x = color)) +
+      geom_bar() 
+    g_color
+  })
+  
+  output$colorTable <- renderTable({
+    tab_color <- table(df_coffee$color)
+    tab_color
+  })
+  
+  output$altitudePlot <- renderPlot({
+    g_altitude <- ggplot(df_coffee, aes(x = altitude_mean_meters)) +
+      geom_histogram() 
+    g_altitude
+  })
+  
+  output$altitudeTable <- renderTable({
+    tab_altitude <- describe(df_coffee$altitude_mean_meters, fast = TRUE) %>%
+      select(-vars)
+    tab_altitude
+  })
+  
+  output$dayPlot <- renderPlot({
+    g_color <- ggplot(df_coffee, aes(x = day)) +
+      geom_bar() 
+    g_color
+  })
+  
+  output$dayTable <- renderTable({
+    tab_day <- table(df_coffee$day)
+    tab_day
+  })
+  
+  output$yearPlot <- renderPlot({
+    g_year <- ggplot(df_coffee, aes(x = year)) +
+      geom_bar() 
+    g_year
+  })
+  
+  output$yearTable <- renderTable({
+    tab_year <- table(df_coffee$year)
+    tab_year
+  })
 })
+
